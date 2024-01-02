6884ebd459437642fbfd0541765f7393cddcc37d
Luka Jaković <ljakovi98@gmail.com>
2024-01-02 15:02:52 +0100

    init project

---
+++ src/main/java/hr/ljakovic/sportsfinder/SportsfinderApplication.java
@@ -0,0 +1,13 @@
+package hr.ljakovic.sportsfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/ljakovic/sportsfinder/SportsfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.ljakovic.sportsfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
